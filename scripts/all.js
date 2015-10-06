9340ea6fb309eda523587f5a117a75032539d74e
Rita.Wy_Wang <ritawy_wang@ritawy-wang.htctaipei.htc.com.tw>
2015-10-06 18:10:37 +0800

    add to do list page

--- scripts/all.js
+++ scripts/all.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $("#shift_burron").click(function(){
         $(".shift").toggle();
     });    
+
+    $(".goBack").click(function(){
+    	window.history.back();
+	});
 });
